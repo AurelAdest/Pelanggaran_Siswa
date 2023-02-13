de1974a4ae4ab33b5e4a78ddfce068a1a93cb668
AurelAdest <101535193+AurelAdest@users.noreply.github.com>
2023-02-13 15:28:26 +0700

    Add files via upload

---
+++ user.js
@@ -0,0 +1,144 @@
+const express = require("express")
+const router = express.Router()
+const db = require("./db")
+
+// end-point akses data user
+router.get("/user", (req, res) => {
+    // create sql query
+    let sql = "select * from user"
+
+    // run query
+    db.query(sql, (error, result) => {
+        let response = null
+        if (error) {
+            response = {
+                message: error.message // pesan error
+            }
+        } else {
+            response = {
+                count: result.length, // jumlah data
+                user: result // isi data
+            }
+        }
+        res.json(response) // send response
+    })
+})
+
+// end-point akses data user berdasarkan id_user tertentu
+router.get("/user/:id", (req, res) => {
+    let data = {
+        id_user: req.params.id
+    }
+    // create sql query
+    let sql = "select * from user where ?"
+
+    // run query
+    db.query(sql, data, (error, result) => {
+        let response = null
+        if (error) {
+            response = {
+                message: error.message // pesan error
+            }
+        } else {
+            response = {
+                count: result.length, // jumlah data
+                user: result // isi data
+            }
+        }
+        res.json(response) // send response
+    })
+})
+
+// end-point menyimpan data siswa
+router.post("/user", (req, res) => {
+
+    // prepare data
+    let data = {
+        nama_user: req.body.nama_user,
+        username: req.body.username,
+        password: req.body.password
+    }
+
+    // create sql query insert
+    let sql = "insert into user set?"
+
+    // run query
+    db.query(sql, data, (error, result) => {
+        let response = null
+        if (error) {
+            response = {
+                message: error.message
+            }
+        } else {
+            response = {
+                message: result.affectedRows + " data inserted"
+            }
+        }
+        res.json(response) // send response
+    })
+})
+
+// end-point mengubah data user
+router.put("/user", (req, res) => {
+
+    // prepare data
+    let data = [
+        // data
+        {
+            nama_user: req.body.nama_user,
+            username: req.body.username,
+            password: req.body.password
+        },
+
+        // parameter (primary key)
+        {
+            id_user: req.body.id_user
+        }
+    ]
+
+    // create sql query update
+    let sql = "update user set ? where ?"
+
+    // run query
+    db.query(sql, data, (error, result) => {
+        let response = null
+        if (error) {
+            response = {
+                message: error.message
+            }
+        } else {
+            response = {
+                message: result.affectedRows + " data updated"
+            }
+        }
+        res.json(response) // send response
+    })
+})
+
+// end-point menghapus data siswa berdasarkan id_siswa
+router.delete("/user/:id", (req, res) => {
+    // prepare data
+    let data = {
+        id_user: req.params.id
+    }
+
+    // create query sql delete
+    let sql = "delete from user where ?"
+
+    // run query
+    db.query(sql, data, (error, result) => {
+        let response = null
+        if (error) {
+            response = {
+                message: error.message
+            }
+        } else {
+            response = {
+                message: result.affectedRows + "data deleted"
+            }
+        }
+        res.json(response) // send response
+    })
+})
+
+module.exports = router
